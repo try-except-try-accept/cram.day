25f7db7194dcbdb931c289111de7c924b74d10a6
unknown <chall168421@gmail.com>
2022-03-25 00:03:16 +0700

    dodgy animations, basic functionality working

---
+++ main.py
@@ -0,0 +1,195 @@
+from flask import Flask, session, url_for, render_template, request, Markup, jsonify
+import json
+import gspread
+from random import choice, randrange, shuffle, sample, randint
+from uuid import uuid4
+
+app = Flask(__name__)
+app.secret_key = uuid4().hex
+
+#############################################################################
+
+def get_stats():
+    '''Read database and return leaderboard'''
+
+    return leaderboard
+
+#############################################################################
+
+def record_answers():
+    '''Write most recent 5 answers to database'''
+
+    pass
+
+
+
+
+#############################################################################
+
+def get_data(year=None, topics=None):
+    '''Get source data from google spreadsheet'''
+    gc = gspread.service_account(filename="secret/cram-revision-app-7385639ec69b.json")
+    sh = gc.open('CRAM Data Source')
+
+    fill_gaps = sh.worksheet('fill_gaps')
+    data = fill_gaps.get('A2:1000')
+
+    source_data = []
+
+    misnomers = set()
+
+    for row in data:
+        if topics is None or row[2] in topics:
+            this_question = {}
+            this_question['question'] = row[0]
+            this_question['keywords'] = row[1].split(", ")
+            this_question['topic'] = row[2]
+            source_data.append(this_question)
+
+        for word in row[1].split(", "):
+            if len(word):
+                misnomers.add(word)
+
+    session['misnomers'] = list(misnomers)
+
+    print(misnomers)
+
+    session['source_data'] = source_data
+
+
+#############################################################################
+
+def create_question():
+    '''Choose random question based on session params'''
+
+
+    source_data = session['source_data']
+
+    if len(source_data) == 0:
+        return "<p>You've answered all questions on your chosen topic.</p>"
+
+    i = randrange(0, len(source_data))
+    question = session['source_data'].pop(i)
+
+
+    replacements = []
+    kw = question['keywords']
+    for i in range(session['difficulty']):
+        replacements.append(kw.pop(randrange(0, len(kw))))
+
+    session['correct'] = []
+
+    question = question['question']
+
+    html_out = ""
+
+    for word in question.split(" "):
+        for rep in replacements:
+            if rep in word:
+                session['correct'].append(rep)
+                html_out += f' <input class="gap_textfield" type="text" name="answer{i}">'
+            else:
+                html_out += " " + word
+
+
+    session['question'] = question
+
+    return Markup(html_out)
+
+#############################################################################
+
+@app.route("/submit_answer", methods=["POST"])
+def submit_answer():
+    '''Check answer and return feedback'''
+
+    answers = request.form.get("answers").split(",")
+
+    question = session['question']
+    replacements = session['correct']
+
+    score = []
+    feedback = []
+    html_out = ""
+    for correct_answer in replacements:
+
+        if len(answers) and correct_answer == answers.pop(0):
+            feedback.append(None)
+            score.append(1)
+        else:
+            feedback.append(correct_answer)
+            score.append(0)
+
+
+    session['scores'].append(round(sum(score)/len(score)))
+
+    response = {'feedback':feedback,
+                'scores':score,
+                'next_question':create_question()}
+
+
+    print("returned response")
+    return jsonify(response)
+
+#############################################################################
+
+@app.route("/get_question", methods=["GET", "POST"])
+def get_question():
+
+    out = "<p>So you wanna study</p>"
+
+    if request.method == "POST":
+        topics = request.form.get("selected_topics").split(",")
+        print("topics is", topics)
+        get_data(topics)
+
+    return create_question()
+
+#############################################################################
+
+@app.route("/fill_the_gaps", methods=["GET"])
+def fill_the_gaps():
+
+    if session.get('scores') is None:
+        session['scores'] = []
+        session['difficulty'] = 1
+
+
+    return render_template("fill_the_gaps.html")
+
+#############################################################################
+
+@app.route("/get_hints", methods=["GET"])
+def get_hints():
+
+    hints = session['correct']
+
+    misnomers = session['misnomers']
+
+    for i in range(6, 10):
+
+        misnomer = choice(misnomers)
+        hints.append(misnomer)
+
+    shuffle(hints)
+
+
+    final_hints = []
+
+    for h in hints:
+        this_hint = {}
+        this_hint['text'] = h + "... ?"
+        this_hint['colour'] = "#" + (hex(randrange(25, 200))[2:].zfill(2) * 3)
+        this_hint['x'] = randrange(0, 800)
+        this_hint['y'] = randrange(400, 800)
+        final_hints.append(this_hint)
+        print(final_hints)
+
+
+    return jsonify(final_hints)
+
+#############################################################################
+
+
+
+
+app.run()
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -65,75 +65,227 @@ function get_topic_list()
 
     let topic_checkbox_display = document.getElementById("topic_checkbox_display");
 
-    let checkboxes = "";
-
-
+    topic_checkbox_display.innerHTML = '';
 
     for (let topic of source)
     {
         topic_id = topic.split(" ");
+        topic_index = topic_id[0];
         topic_id = topic_id.slice(1, topic.length);
         topic_id = topic_id.join("").replace(" ", "_").toLowerCase();
 
-        checkboxes += `<div class="topic_checkbox"><input type="checkbox" id="${topic_id}" name="${topic_id}" checked>  <label for="${topic_id}">${topic}</label></div>`;
+        //checkboxes += `<div class="topic_checkbox"><input type="checkbox" class="topic_checkbox" id="topic_${topic_id}" name="topic_${topic_index}" checked>  <label for="topic_${topic_id}">${topic}</label></div>`;
 
+
+        let checkbox_div = document.createElement("div");
+        let checkbox = document.createElement("input");
+        let label = document.createElement("label");
+
+        checkbox_div.appendChild(checkbox);
+        checkbox_div.appendChild(label);
+
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("class", "topic_checkbox");
+        checkbox.setAttribute("checked", true);
+        checkbox.setAttribute("name", topic_index);
+        checkbox.setAttribute("id", "topic_"+topic_id);
+        label.setAttribute("for", "topic_"+topic_id);
+        label.innerHTML = topic;
+
+        topic_checkbox_display.appendChild(checkbox_div);
 
     }
-
-    topic_checkbox_display.innerHTML = checkboxes;
 
 
 }
 
 
-	function get_stats()
-	{
-		fetch('/get_stats')
-		.then(res => {
-    	return res.text()})
-		.then(data => {
-		$('#stats_modal_header').html('<h4 class="modal-title">' + JSON.parse(data).display_name + "'s Stats</h4>");
-		$('#stats_modal_content').html(JSON.parse(data).stats);
+function get_stats()
+{
+    fetch('/get_stats')
+    .then(res => {
+    return res.text()})
+    .then(data => {
+    $('#stats_modal_header').html('<h4 class="modal-title">' + JSON.parse(data).display_name + "'s Stats</h4>");
+    $('#stats_modal_content').html(JSON.parse(data).stats);
 
-    	}
-    	);
-    };
-
-    function get_question()
-	{
-		fetch('/get_question')
-		.then(res => {
-    	return res.text()})
-		.then(data => {
-		$('#stats_modal_header').html('<h4 class="modal-title">' + JSON.parse(data).display_name + "'s Stats</h4>");
-		$('#stats_modal_content').html(JSON.parse(data).stats);
-
-    	}
-    	);
-    };
-
-    function submit_answer()
-	{
-		console.log("reporting problem");
-		let form_data = new FormData();
-		form_data.append('q_num', $("input#q_num").val());
-
-		console.log(form_data);
-
-		fetch('/submit_answer',
-		{
-			body:form_data,
-			method:"post"
-		})
-		.then(res => {
-    	return res.text()})
-		.then(data => {
-		 $('#report_problem_modal').modal('toggle');
-		//$('#msgs').html(data).show().delay(3000).fadeOut(3000);
-		$('#msgs').append(data);
-    	}
-    	);
-
-    };
+    }
+    );
+};
 
 
+function process_hints(hints)
+{
+    for (let h of hints)
+    {
+         let new_hint = document.createElement("div")
+         new_hint.innerHTML = h.text;
+         new_hint.classList.add("hint")
+         new_hint.style.top = h.y.toString() + "px";
+         new_hint.style.left = h.x.toString() + "px";
+         new_hint.style.color = h.colour;
+
+         document.getElementsByTagName("body")[0].appendChild(new_hint);
+    }
+}
+
+function remove_hints()
+{
+    let body = document.getElementsByTagName("body")[0];
+
+    $('.hint').each(function(i, obj)
+    {
+        let option = Math.floor(Math.random() * 10);
+
+        if (option > 7) { this.animate({"top":"0"}, 1000, () => this.fadeOut(100)) }
+        else if (option > 5) { this.animate({"top":"1000px"}, () => this.fadeOut(100) )}
+        else if (option > 2) { this.animate({"left":"1000px"}, () => this.fadeOut(100) )}
+        else  { this.animate({"top":"1000px", "left":"1000px"}, () => this.fadeOut(100)) } ;
+    });
+}
+
+function process_feedback(feedback, scores)
+{
+
+    console.log("Feedback is");
+    console.log(feedback);
+    feedback_div = document.getElementById("feedback");
+    question_div = document.getElementById("question");
+
+    feedback_div.innerHTML = "";
+
+    for (let child of question_div.childNodes
+    )
+    {
+        let new_node = child.cloneNode()
+        try
+        {
+            if (child.getAttribute("class") == "gap_textfield")
+            {
+
+                let next_correct = feedback.pop(0);
+                console.log("next correct" + next_correct);
+                let text_input_feedback = "";
+                if (next_correct != null)
+                {
+                   text_input_feedback = "✘" + child.value + "✔" + next_correct;
+
+                }
+                else
+                {
+                   text_input_feedback = "✔" + child.value;
+                }
+
+                new_node.value = text_input_feedback;
+
+            }
+        }
+        catch (e) { console.log("problemmm" + e)}
+
+        feedback_div.appendChild(new_node);
+    }
+
+    for (let i=0; i<feedback_div.children.length; i++)
+    {
+        let element = feedback_div.children[i];
+        let grading = scores[i];
+
+        element.setAttribute("disabled", true);
+        let colour = "incorrect";
+        if (grading) { colour = "correct"}
+        element.classList.add(colour);
+    }
+
+
+    const average = array => array.reduce( ( p, c ) => p + c, 0 ) / array.length;
+
+
+    if (average(scores) == 1) { return 0 }
+    else if (average(scores) == 0) { return 1000 }
+    else { return -1}
+
+
+
+
+}
+
+function submit_answer()
+{
+    let form_data = new FormData();
+    answers = document.getElementsByClassName("gap_textfield");
+    answers_given = [];
+    for (let a of answers)
+    {
+        answers_given.push(a.value);
+    }
+    form_data.append("answers", answers_given);
+
+    fetch('/submit_answer',
+    {
+        body:form_data,
+        method:"post"
+    })
+    .then(res => {
+    return res.json()})
+    .then(data => {
+
+
+
+    console.log("Received data");
+    console.log(data);
+    remove_hints();
+    let fb_anim = process_feedback(data.feedback, data.scores);
+    console.log("feedback anim");
+    console.log(fb_anim);
+    if (fb_anim != -1) { anim_actions = {"top":fb_anim.toString(), "opacity":0} }
+    else { anim_actions = {"left":"1000", "opacity":0} };
+
+
+    $("#feedback").css({"opacity":1, "left":"0px", "top":"200px"});
+    $("#feedback").fadeOut(0);
+    $("#feedback").fadeIn(1000,
+        () => $("#question").html(data.next_question).fadeIn(100,
+            () => $("#feedback").animate(anim_actions, 1000, () => null)
+        )
+    );
+
+    }
+    );
+};
+
+
+function get_question()
+{
+    let form_data = new FormData();
+    topics = document.getElementsByClassName("topic_checkbox");
+    selected_topics = [];
+    for (let t of topics)
+    {
+        if (t.checked) { selected_topics.push(t.name)}
+    }
+    form_data.append("selected_topics", selected_topics);
+    console.log(form_data);
+
+    fetch('/get_question',
+    {
+        body:form_data,
+        method:"post"
+    })
+    .then(res => {
+    return res.text()})
+    .then(data => {
+     $('#choose_topics_modal').modal('toggle');
+
+    document.getElementById("question").innerHTML = data;
+
+    }
+    );
+};
+
+function get_hints()
+{
+    fetch('/get_hints')
+    .then(res => {
+    return res.json()})
+    .then( data => process_hints(data));
+};
